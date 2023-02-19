f387513e72de67fd461730d9975c2aca57ef0d73
AnnaSarYan <annasargsyan527.527@gmail.com>
2023-02-20 01:27:16 +0400

    Add all project

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'fb18c78548msh0668646a87ec963p1b6135jsn52cb0578afee',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "327ad6b585345718f2ff8a0eacb7d895";
